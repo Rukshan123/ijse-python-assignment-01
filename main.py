fba15623fbd294e088412bf5d9ce53571e082bd1
Rukshan123 <rukzmadu@gmail.com>
2021-11-01 10:24:49 +0530

    set db initialize files

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import sys
 import json
 import os
 from pprint import pprint
+
+__db_location__ = "db"
+__session_file__  = f"{__db_location__}/session.db"
+__item_folder__  =  f"{__db_location__}/item"
+__item_last_id__  =  f"{__db_location__}/item_id.db"
