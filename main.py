724472cd814182a361bf4d031bb6af78483ffc4a
Rukshan123 <rukzmadu@gmail.com>
2021-11-01 10:41:43 +0530

    clear all

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import json
-import os
-from pprint import pprint
-
-__db_location__ = "db"
-__session_file__  = f"{__db_location__}/session.db"
-__item_folder__  =  f"{__db_location__}/item"
-__item_last_id__  =  f"{__db_location__}/item_id.db"
