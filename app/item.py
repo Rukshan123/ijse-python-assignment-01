26f940e9b2f4903d8fc21e2eea7c0fe2f80a7b8c
Rukshan123 <rukzmadu@gmail.com>
2021-11-03 10:53:10 +0530

    update item db file location

--- app/item.py
+++ app/item.py
@@ -6,7 +6,7 @@ from pprint import pprint
 __db_location__ = "db"
 __session_file__  = f"{__db_location__}/session.db"
 __item_folder__  =  f"{__db_location__}/item"
-__item_last_id__  =  f"{__db_location__}/item_id.db"
+__item_last_id__  =  f"{__db_location__}/item/item_id.db"
 
 def init(arguments):
 
